cafcd9c7bc09dff95a5dbf386fe7c52f139c8ecd
spoolglans <jerry@jerryalmen.se>
2021-12-03 00:09:37 +0100

    Moved day 2 to a single file

--- day2.py
+++ day2.py
@@ -5,10 +5,27 @@ lines = input.open('r').readlines()
 
 depth = 0
 hpos = 0
+
+for line in lines:
+    dir, steps = line.strip().rsplit()
+    steps = int(steps)
+    if dir == 'forward':
+        hpos += steps
+    elif dir == 'down':
+        depth += steps
+    elif dir == 'up':
+        depth -= steps
+    else:
+        print('No command found')
+
+print(depth * hpos)
+
+depth = 0
+hpos = 0
 aim = 0
 
 for line in lines:
-    dir, steps = line.strip().rsplit(' ')
+    dir, steps = line.strip().rsplit()
     steps = int(steps)
     if dir == 'forward':
         depth += steps * aim
