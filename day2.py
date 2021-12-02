1637d2a177727a68f335a11079957adf876890fe
spoolglans <jerry@jerryalmen.se>
2021-12-03 00:22:44 +0100

    day2 made in to functions

--- day2.py
+++ day2.py
@@ -3,38 +3,44 @@ from pathlib import Path
 input = Path(__file__).with_name('input2.txt')
 lines = input.open('r').readlines()
 
-depth = 0
-hpos = 0
+def diveSimple (data):
+    depth = 0
+    hpos = 0
 
-for line in lines:
-    dir, steps = line.strip().rsplit()
-    steps = int(steps)
-    if dir == 'forward':
-        hpos += steps
-    elif dir == 'down':
-        depth += steps
-    elif dir == 'up':
-        depth -= steps
-    else:
-        print('No command found')
+    for command in data:
+        dir, steps = command.strip().rsplit()
+        steps = int(steps)
+        if dir == 'forward':
+            hpos += steps
+        elif dir == 'down':
+            depth += steps
+        elif dir == 'up':
+            depth -= steps
+        else:
+            print('No command found')
 
-print(depth * hpos)
+    return depth * hpos
 
-depth = 0
-hpos = 0
-aim = 0
+def diveComplex (data):
+    depth = 0
+    hpos = 0
+    aim = 0
 
-for line in lines:
-    dir, steps = line.strip().rsplit()
-    steps = int(steps)
-    if dir == 'forward':
-        depth += steps * aim
-        hpos += steps
-    elif dir == 'down':
-        aim += steps
-    elif dir == 'up':
-        aim -= steps
-    else:
-        print('No command found')
-    
-print(depth * hpos)
+    for command in data:
+        dir, steps = command.strip().rsplit()
+        steps = int(steps)
+        if dir == 'forward':
+            depth += steps * aim
+            hpos += steps
+        elif dir == 'down':
+            aim += steps
+        elif dir == 'up':
+            aim -= steps
+        else:
+            print('No command found')
+        
+    return depth * hpos
+
+
+print('Dive version 1 result: ' + str(diveSimple(lines)))
+print('Dive version 2 result: ' + str(diveComplex(lines)))
