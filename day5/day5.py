45e198bb80a09344430602c949251bdc004e5130
spoolglans <jerry@jerryalmen.se>
2021-12-05 06:31:26 +0100

    day5 prep

---
+++ day5/day5.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from re import findall
+from math import sqrt
+
+input = Path(__file__).with_name('day5_test.txt')
+lines = input.open('r').read().split('\n')
+
+class bingo_board():
+    def __init__(self) -> None:
+        pass
