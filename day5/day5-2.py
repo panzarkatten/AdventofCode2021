a4cb75b0384da002e96bf8062ca7d0ff31ee9b6e
spoolglans <jerry@jerryalmen.se>
2021-12-07 22:57:37 +0100

    I hate coordinates...

--- day5/day5-2.py
+++ day5/day5-2.py
@@ -27,15 +27,16 @@ for coord in coords:
     b1 = int(b1)
     b2 = int(b2)
 
-    if a1 == a2 or b1 == b2:
-        x1, x2 = min(a1,a2),max(a1,a2)
-        y1, y2 = min(b1,b2),max(b1,b2)
 
-        # print(f'x1: {x1}', end=' ')
-        # print(f'y1: {y1}')
-        # print(f'x2: {x2}', end=' ')
-        # print(f'y2: {y2}')
+    x1, y1 = min(a1,a2),min(b1,b2)
+    x2, y2 = max(a1,a2),max(b1,b2)
 
+    # print(f'x1: {x1}', end=' ')
+    # print(f'y1: {y1}')
+    # print(f'x2: {x2}', end=' ')
+    # print(f'y2: {y2}')
+
+    if x1 == x2 or y1 == y2:
         for x in range(x1,x2+1):
             for y in range(y1,y2+1):        
                 # print(f'x{x},y{y}')
@@ -44,37 +45,11 @@ for coord in coords:
                 
                 lines[(x,y)] += 1
     else:
-        if a1 < a2:
-            xstep = 1
-        else:
-            xstep = -1
-
-        if b1 < b2:
-            ystep = 1
-        else:
-            ystep = -1
-
-        x1,y1,x2,y2 = a1,b1,a2,b2
         
-        x,y = x1,y1
-        
-        # print(f'x1: {x1}', end=' ')
-        # print(f'y1: {y1}')
-        # print(f'x2: {x2}', end=' ')
-        # print(f'y2: {y2}')
-        while x != x2:
-            # print(f'Diagonal line {x1},{y1} to {x2},{y2}: {x},{y}')
-            if (x,y) not in lines:
-                lines[(x,y)] = 0
-            
-            lines[(x,y)] += 1
 
-            x += xstep
-            y += ystep
 
 for k,v in lines.items():
     if v > 1:
-        print(k,v,sep=' : ')
         intersects += 1
 
 # print(lines)
---
+++ utils/coordinates.py
@@ -0,0 +1,13 @@
+
+
+# Takes a start x,y and an end x,y and returns all coordinates between them
+def get_line_cords(line):
+    pass
+
+# Takes two lines and returns the coordinate where they intersect. Returns None if they don't intersect
+def get_line_intersect(line1, line2):
+    pass
+
+def line_is_diagonal(line):
+    pass
+
