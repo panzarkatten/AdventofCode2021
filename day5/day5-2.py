abb5195049c97f934ab206edf705967ecff0c83c
spoolglans <jerry@jerryalmen.se>
2021-12-08 01:38:28 +0100

    day5 part 2 solved. Need cleanup.

--- day5/day5-2.py
+++ day5/day5-2.py
@@ -1,58 +1,81 @@
 from pathlib import Path
-from re import findall
-from math import sqrt
+from typing import Tuple
+
+def process_input(input):
+    lines = input.replace(' ','').split('\n')
+    # cords = [c.split('->') for c in cords]
+    cords = ()
+    for line in lines:
+        print(line.split('->'))
+        cord = line.split('->')
+        cord = [tuple(map(int, c.split(','))) for c in cord]
+        cords += tuple(cord)
+
+    return tuple(cords)
+
+def get_line_cords(cords: Tuple[Tuple[int, int], Tuple[int, int]]):
+    lines = ()
+    iterations = iter(cords)
+
+    for cord in iterations:
+        x1, y1 = cord
+        x2, y2 = next(iterations)
+        line = ()
+        
+        if x1 == x2 or y1 == y2:
+            # Vertical or horizontal line
+            x1, x2 = min(x1,x2),max(x1,x2)
+            y1, y2 = min(y1,y2),max(y1,y2)
+
+            for x in range(x1,x2+1):
+                for y in range(y1,y2+1):        
+                    print(f'x{x},y{y}')
+                    line += ((x,y),)
+        else:
+            # Diagonal line
+            if x1 < x2:
+                xstep = 1
+                x2 += 1
+            else:
+                xstep = -1
+                x2 -= 1
+
+            if y1 < y2:
+                ystep = 1
+                y2 += 1
+            else:
+                ystep = -1
+                y2 -= 1
+
+            x = list(range(x1, x2, xstep))
+            y = list(range(y1, y2, ystep))
+
+            line = tuple(zip(x,y))
+        
+        lines += (line,)
+    
+    return lines
+
+# Takes two lines and returns the coordinate where they intersect. Returns None if they don't intersect
+def get_line_intersects(lines):
+    intersects = {}
+    for line in lines:
+        for cord in line:
+            x,y = cord
+            if (x,y) not in intersects:
+                intersects[(x,y)] = 0
+            
+            intersects[(x,y)] += 1
+    
+    return [k for k,v in intersects.items() if v > 1]
+
 
 input = Path(__file__).with_name('day5.txt')
 input = input.open('r').read()
 
+cords = process_input(input)
 
-def process_input(input):
-    coords = input.replace(' ','').split('\n')
-    coords = [tuple(c.split('->')) for c in coords]
+lines = get_line_cords(cords)
+intersections = get_line_intersects(lines)
 
-    return tuple(coords)
-
-coords = process_input(input)
-# print(coords)
-
-lines = {}
-intersects = 0
-
-for coord in coords:
-    a1, b1 = coord[0].split(',')
-    a2, b2 = coord[1].split(',')
-
-    a1 = int(a1)
-    a2 = int(a2)
-    b1 = int(b1)
-    b2 = int(b2)
-
-
-    x1, y1 = min(a1,a2),min(b1,b2)
-    x2, y2 = max(a1,a2),max(b1,b2)
-
-    # print(f'x1: {x1}', end=' ')
-    # print(f'y1: {y1}')
-    # print(f'x2: {x2}', end=' ')
-    # print(f'y2: {y2}')
-
-    if x1 == x2 or y1 == y2:
-        for x in range(x1,x2+1):
-            for y in range(y1,y2+1):        
-                # print(f'x{x},y{y}')
-                if (x,y) not in lines:
-                    lines[(x,y)] = 0
-                
-                lines[(x,y)] += 1
-    else:
-        
-
-
-for k,v in lines.items():
-    if v > 1:
-        intersects += 1
-
-# print(lines)
-print(len(lines))
-print(lines)
-print(intersects)
+print(len(intersections))
--- utils/coordinates.py
+++ utils/coordinates.py
@@ -1,13 +1,64 @@
-
+from typing import List, Tuple
 
 # Takes a start x,y and an end x,y and returns all coordinates between them
-def get_line_cords(line):
-    pass
+def get_line_cords(cords: Tuple[Tuple[int, int], Tuple[int, int]]):
+    lines = ()
+    iterations = iter(cords)
+
+    for cord in iterations:
+        x1, y1 = cord
+        x2, y2 = next(iterations)
+        line = ()
+        
+        if x1 == x2 or y1 == y2:
+            # Vertical or horizontal line
+            x1, x2 = min(x1,x2),max(x1,x2)
+            y1, y2 = min(y1,y2),max(y1,y2)
+
+            for x in range(x1,x2+1):
+                for y in range(y1,y2+1):        
+                    print(f'x{x},y{y}')
+                    line += ((x,y),)
+        else:
+            # Diagonal line
+            if x1 < x2:
+                xstep = 1
+                x2 += 1
+            else:
+                xstep = -1
+                x2 -= 1
+
+            if y1 < y2:
+                ystep = 1
+                y2 += 1
+            else:
+                ystep = -1
+                y2 -= 1
+
+            x = list(range(x1, x2, xstep))
+            y = list(range(y1, y2, ystep))
+
+            line = tuple(zip(x,y))
+        
+        lines += (line,)
+    
+    return lines
 
 # Takes two lines and returns the coordinate where they intersect. Returns None if they don't intersect
-def get_line_intersect(line1, line2):
-    pass
+def get_line_intersects(lines):
+    intersects = {}
+    for line in lines:
+        for cord in line:
+            x,y = cord
+            if (x,y) not in intersects:
+                intersects[(x,y)] = 0
+            
+            intersects[(x,y)] += 1
+    
+    return [k for k,v in intersects.items() if v > 1]
 
-def line_is_diagonal(line):
-    pass
+lines = get_line_cords( ((4,0), (0,4), (0,0), (4,4)) )
+intersects = get_line_intersects(lines)
 
+print(f'Line: {lines}')
+print(f'Intersections: {intersects}')
