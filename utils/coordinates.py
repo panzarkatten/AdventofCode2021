298881f8b900bbd559c416afc2a59401d1b7ce09
spoolglans <jerry@jerryalmen.se>
2021-12-09 00:17:21 +0100

    cleaup

--- utils/coordinates.py
+++ utils/coordinates.py
@@ -1,6 +1,5 @@
-from typing import List, Tuple
+from typing import Tuple
 
-# Takes a start x,y and an end x,y and returns all coordinates between them
 def get_line_cords(cords: Tuple[Tuple[int, int], Tuple[int, int]]):
     lines = ()
     iterations = iter(cords)
@@ -17,7 +16,6 @@ def get_line_cords(cords: Tuple[Tuple[int, int], Tuple[int, int]]):
 
             for x in range(x1,x2+1):
                 for y in range(y1,y2+1):        
-                    print(f'x{x},y{y}')
                     line += ((x,y),)
         else:
             # Diagonal line
@@ -44,7 +42,6 @@ def get_line_cords(cords: Tuple[Tuple[int, int], Tuple[int, int]]):
     
     return lines
 
-# Takes two lines and returns the coordinate where they intersect. Returns None if they don't intersect
 def get_line_intersects(lines):
     intersects = {}
     for line in lines:
@@ -56,9 +53,3 @@ def get_line_intersects(lines):
             intersects[(x,y)] += 1
     
     return [k for k,v in intersects.items() if v > 1]
-
-lines = get_line_cords( ((4,0), (0,4), (0,0), (4,4)) )
-intersects = get_line_intersects(lines)
-
-print(f'Line: {lines}')
-print(f'Intersections: {intersects}')
