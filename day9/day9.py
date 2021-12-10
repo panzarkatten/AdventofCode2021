6dc5064e49c3dc6d9a4806efd3d41304eeb177b3
spoolglans <jerry@jerryalmen.se>
2021-12-10 04:30:30 +0100

    Part Two working. Need cleanup.

--- day9/day9.py
+++ day9/day9.py
@@ -1,33 +1,51 @@
 from pathlib import Path
 from typing import Tuple, List, Dict
 
-input = Path(__file__).with_name('day9_test.txt')
+input = Path(__file__).with_name('day9.txt')
 raw_input = input.open('r').read()
 
 def process_input(input: str) -> List[Tuple[Tuple[str, str]]]:
     """Process the input and return an object which is easier to work with"""
-
     data = list([list(map(int, line)) for line in list(input.strip().split('\n'))])
     
     return data
 
-def get_pos_height(pos: Tuple[int, int], map: List[List[int]]):
+def get_pos_height(pos: Tuple[int, int], map):
     x,y = pos
     return map[y][x]
 
-def get_neighbour_pos(pos):
-    x,y = pos
-
-    u = (x     , y - 1)
-    r = (x + 1 , y    )
-    d = (x     , y + 1)
-    l = (x - 1 , y    )
-
-    neighbours_pos = {'u': u,'r': r,'d': d,'l': l}
+def get_neighbours_height(neighbours, map):
+    neighbour_heights = []
+    for p in neighbours:
+        if p:
+            neighbour_heights.append(get_pos_height(p, map))
+        else:
+            neighbour_heights.append(False)
     
-    return {'u': u,'r': r,'d': d,'l': l}
+    return neighbour_heights
 
-print(f'Neighbour positions: {get_neighbour_pos((4,4))}')
+def get_neighbour_pos(pos, map):
+    # Setup
+    x,y = pos
+    map_size = get_map_size(map)
+    map_x_lim = map_size['x'] - 1
+    map_y_lim = map_size['y'] - 1
+
+    # Calculate new positions
+    neighbours_pos = [
+        (x     , y - 1),
+        (x + 1 , y    ),
+        (x     , y + 1),
+        (x - 1 , y    )
+    ]
+
+    # Check if any of the positions is out of bound
+    for i,v in enumerate(neighbours_pos):
+        xn,yn = v
+        if any([xn < 0, xn > map_x_lim, yn < 0, yn > map_y_lim]):
+           neighbours_pos[i] = False 
+
+    return neighbours_pos
 
 def get_neighbours(pos: Tuple[int, int], map: List[List[int]]) -> List[int]:
     """
@@ -47,16 +65,17 @@ def get_neighbours(pos: Tuple[int, int], map: List[List[int]]) -> List[int]:
     map_y_lim = len(map) - 1
 
     # Absolut positions to the neighbours
-    n_pos = get_neighbour_pos(pos)
+    n_pos = get_neighbour_pos(pos, map)
 
     # Handle posisions at the edge of the map
-    for d,p in n_pos.items():
-        xn,yn = p
+    for p in n_pos:
+        if p:
+            xn,yn = p
 
-        if not any([xn < 0, xn > map_x_lim, yn < 0, yn > map_y_lim]):
-            n.append(get_pos_height((xn,yn),map))
-        else:
-            n.append(9)
+            if not any([xn < 0, xn > map_x_lim, yn < 0, yn > map_y_lim]):
+                n.append(get_pos_height((xn,yn),map))
+            else:
+                n.append(9)
             
     return n
 
@@ -66,11 +85,11 @@ def get_map_size(map: List[List[int]]) -> Dict[str, int]:
 
 def get_low_spots(map: List[List[int]]) -> List[Tuple[int, int]]:
     low_spots = []
+    map_size = get_map_size(map)
     for y in range(0, map_size['y']):
         for x in range(0, map_size['x']):
-            print(f'x: {x} \t y: {y}')
-            neighbours = get_neighbours((x,y), flow_map)
-            pos_h = get_pos_height((x,y), flow_map)
+            neighbours = get_neighbours((x,y), map)
+            pos_h = get_pos_height((x,y), map)
 
             if pos_h < min(neighbours):
                 low_spots.append((x,y))
@@ -89,20 +108,62 @@ def get_higher_neighbours(pos: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
     
     return low_spots
 
-def get_basin(pos):
-    pass
+
+def get_basin(pos, map):
+    basin = []
+    if pos:    
+        pos_height = get_pos_height(pos, map)
+        nb_positions = get_neighbour_pos(pos, map)
+        nb_heights = get_neighbours_height(nb_positions, map)
+        print(' ')
+        print(f'Checking position {pos}')
+        print(f'nb_positions: {nb_positions}')
+        print(f'nb_heights: {nb_heights}')
+
+        print(f'Checking all neighbours of {pos}')
+        found_basin = False
+        new_basin = []
+        for i,v in enumerate(nb_heights):
+            
+            print(f'i: {i} \t v: {v}')
+            if v > pos_height and nb_positions[i] and v != 9:
+                print(f'Found new part of basin {nb_positions[i]} ')
+                found_basin = True
+                print(f'Appending {nb_positions[i]} to new_basin')
+                new_basin.append(nb_positions[i])
+                
+
+        print(f'new_basin: {new_basin}')
+
+        if found_basin:
+            print('--- Digging deeper ---')
+            for p in new_basin:
+                if p not in basin:
+                    basin += get_basin(p, map)
+        
+        basin.append(pos)
+
+        basin = list(set(basin))
+
+        return basin
+            
+
+
+
+
 
 # --- Init ---
 flow_map = process_input(raw_input)
-map_size = get_map_size((flow_map))
+# map_size = get_map_size((flow_map))
 low_spots = get_low_spots(flow_map)
 
 # --- Part One ---
-sum = 0
-for p in low_spots:
-    sum += get_pos_height(p, flow_map) + 1
 
-print(f'Part One: {sum}')
+# sum = 0
+# for p in low_spots:
+#     sum += get_pos_height(p, flow_map) + 1
+
+# print(f'Part One: {sum}')
 
 # --- Part Two ---
 """
@@ -111,14 +172,24 @@ print(f'Part One: {sum}')
 9856789892
 8767896789
 9899965678
+
+(5, 0), (6, 0), (7, 0), (8, 0), (9, 0)
+(6, 1), (8, 1), (9, 1)
+(9,2)
 """
 
 # Find position of the lowest points on the map
 
 
 # Go through all points found and get all neighbours that are higher and save in i list
-for ls in low_spots:
-    neighbours = get_neighbour_pos(ls)
-    print(neighbours)
+basins = [get_basin(ls, flow_map) for ls in low_spots]
+
+top3 = list(sorted(basins, key = len, reverse = True))[:3]
+
+l = list(map(len,top3))
+
+ans = l[0] * l[1] * l[2]
+
+print(ans)
 # Iterade thru that list until no more higher points are found (recursion?)
 # In the end you should end up with a list of lists where each list contains the coordinates for that basin.
