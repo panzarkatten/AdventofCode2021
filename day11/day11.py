5008cb33a1f749d8f0b037d69b905ed5aae33d3f
spoolglans <jerry@jerryalmen.se>
2021-12-12 10:37:32 +0100

    Part two has some bug...

--- day11/day11.py
+++ day11/day11.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-input = Path(__file__).with_name('day11.txt')
+input = Path(__file__).with_name('day11_test.txt')
 raw_input = input.open('r').read()
 
 def process_input(input):
@@ -71,14 +71,17 @@ def flash_octopi(grid):
     counter = 0
     triggered = []
     stop = False
+    all_flashed = False
 
     while stop == False:
         trig = False
+        num_flashed = 0
         for x in range(x_lim):
             for y in range(y_lim):
                 d = get_pos_data((x,y), grid)
                 if d >= 10:
                     counter += 1
+                    num_flashed += 1
                     trig = True
                     grid = set_pos_data((x,y), 0, grid)
                     triggered.append((x,y))
@@ -87,16 +90,16 @@ def flash_octopi(grid):
                         if p and p not in triggered:
                             xa, ya = p
                             grid = pos_increase((xa,ya), 1, grid)
-        
+
         if trig == False:
             stop = True
     
-    print(f'Triggered: {triggered}')
-
-    return grid, counter            
+    return grid, counter        
 
 # --- Init ---
 grid = process_input(raw_input)
+flashed_octopi = 0
+step_all_flashed = None
 print(input)
 # --- Part One ---
 print(f'Before any steps:')
@@ -106,22 +109,26 @@ for r in grid:
     print('')
 print('\n')
 
-flashed_octopi = 0
+
 for i in range(100):
     # Increase all positions with one
     grid = grid_increase(grid, 1)
     grid, flashed = flash_octopi(grid)
     flashed_octopi += flashed
 
-    print(f'After step {i + 1}:')
-    for r in grid:
-        for c in r:
-            print(c, end='')
-    
-        print('')
-    print('\n')
-
-print(f'Total flashed: {flashed_octopi}')
+print(f'Total flashed after 100 steps: {flashed_octopi}')
 
 
 # --- Part Two ---
+step = 1
+while step_all_flashed == None:
+    # Increase all positions with one
+    grid = grid_increase(grid, 1)
+    grid, flashed = flash_octopi(grid)
+    print(f'Flashed octopi at step {step}: {flashed}')
+    step += 1
+
+    if flashed == 110 or step == 200:
+        step_all_flashed = step
+
+print(f'Step all flashed: {step_all_flashed}')
