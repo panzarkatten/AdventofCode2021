795666e9c09109b792371461f657848daccddd3a
spoolglans <jerry@jerryalmen.se>
2021-12-14 00:36:47 +0100

    Input processed

--- day13/day13.py
+++ day13/day13.py
@@ -1,124 +1,40 @@
 from pathlib import Path
 
-input = Path(__file__).with_name('day11.txt')
+input = Path(__file__).with_name('day13_test.txt')
 raw_input = input.open('r').read()
 
 def process_input(input):
     """Process the input and return an object which is easier to work with"""
-    data = list([list(map(int, line)) for line in list(input.strip().split('\n'))])
-    
-    return data
+    data = input.split('\n\n')
 
-def get_adj_pos(pos, grid):
-    # Setup
-    x,y = pos
-    grid_size = get_grid_size(grid)
-    x_lim = grid_size['x'] - 1
-    y_lim = grid_size['y'] - 1
+    in1 = data[0].split('\n')
+    dots = []
+    for dot in in1:
+        x,y = dot.split(',')
+        x = int(x)
+        y = int(y)
+        dots.append(tuple((x,y)))
 
-    # Calculate new positions
-    neighbours_pos = [
-        (x     , y - 1), # N
-        (x + 1 , y - 1), # NE
-        (x + 1 , y    ), # E
-        (x + 1 , y + 1), # SE
-        (x     , y + 1), # S
-        (x - 1 , y + 1), # SW
-        (x - 1 , y    ), # W
-        (x - 1 , y - 1)  # NW
-    ]
+    in2 = data[1].split('\n')
+    folds = []
+    for fold in in2:
+        d,n = fold.split()[2].split('=')
+        n = int(n)
+        folds.append(tuple((d,n)))
 
-    # Check if any of the positions is out of bound
-    for i,v in enumerate(neighbours_pos):
-        xn,yn = v
-        if any([xn < 0, xn > x_lim, yn < 0, yn > y_lim]):
-           neighbours_pos[i] = False 
+    return dots, folds
 
-    return neighbours_pos
-
-def get_pos_data(pos, grid):
-    x,y = pos
-    return grid[y][x]
-
-def set_pos_data(pos, data, grid):
-    x,y = pos
-    grid[y][x] = data
-    return grid
-
-def get_grid_size(grid):
-    return {'x': len(grid[0]), 'y': len(grid)}
-
-def pos_increase(pos, num, grid):
-    x,y = pos
-    grid[y][x] += num
-    return grid
-
-def grid_increase(grid, num):
-    grid_size = get_grid_size(grid)
-    x_lim = grid_size['x']
-    y_lim = grid_size['y']
-    
-    for y in range(y_lim):
-        for x in range(x_lim):
-            grid[y][x] += num
-    
-    return grid
-
-def flash_octopi(grid):
-    grid_size = get_grid_size(grid)
-    x_lim = grid_size['x']
-    y_lim = grid_size['y']
-    counter = 0
-    triggered = []
-    stop = False
-
-    while stop == False:
-        trig = False
-        num_flashed = 0
-        for x in range(x_lim):
-            for y in range(y_lim):
-                d = get_pos_data((x,y), grid)
-                if d >= 10:
-                    counter += 1
-                    num_flashed += 1
-                    trig = True
-                    grid = set_pos_data((x,y), 0, grid)
-                    triggered.append((x,y))
-                    adj = get_adj_pos((x,y), grid)
-                    for p in adj:
-                        if p and p not in triggered:
-                            xa, ya = p
-                            grid = pos_increase((xa,ya), 1, grid)
-
-        if trig == False:
-            stop = True
-    
-    return grid, counter        
+      
 
 # --- Init ---
-grid = process_input(raw_input)
-flashed_octopi = 0
-step_all_flashed = None
-print(input)
-# --- Part One ---
-for i in range(100):
-    # Increase all positions with one
-    grid = grid_increase(grid, 1)
-    grid, flashed = flash_octopi(grid)
-    flashed_octopi += flashed
+dots, folds = process_input(raw_input)
 
-print(f'Part One: {flashed_octopi}')
+print(dots, folds, sep='\n')
+# --- Part One ---
+
+print(f'Part One: ')
 
 
 # --- Part Two ---
-grid = process_input(raw_input)
-step = 1
 
-while step_all_flashed == None:
-    grid = grid_increase(grid, 1)
-    grid, flashed = flash_octopi(grid)
-    if flashed == 100:
-        step_all_flashed = step
-    step += 1
-
-print(f'Part Two: {step_all_flashed}')
+print(f'Part Two: ')
